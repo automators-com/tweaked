5b123e20dbd616f1d7ec45584abfc1854ca309cb
Ratul Maharaj <56479869+RatulMaharaj@users.noreply.github.com>
2024-06-02 22:20:33 +0200

    style: :lipstick: Add automators theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,23 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "garden"],
+    themes: [
+      {
+        automators: {
+          primary: "#FFFFFF",
+          secondary: "#482B7D",
+          accent: "#449BA7",
+          neutral: "#F2F2F2",
+          "base-100": "#1B1C36",
+          "base-200": "#282943",
+          "base-300": "#282943",
+        },
+      },
+      "light",
+      "dark",
+      "cupcake",
+      "garden",
+    ],
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
