b7e5243fc47dab1d69a7269e6af1da87b8ea8a82
Ratul Maharaj <56479869+RatulMaharaj@users.noreply.github.com>
2024-06-17 11:35:43 +0200

    feat: :construction: Setup custom menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,43 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri_plugin_updater;
+use tauri::menu::{AboutMetadata, Menu, MenuItem, PredefinedMenuItem, Submenu};
 use tauri_plugin_dialog;
 use tauri_plugin_process;
+use tauri_plugin_updater;
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
+            let handle = app.handle();
             #[cfg(desktop)]
-            app.handle().plugin(tauri_plugin_updater::Builder::new().build())?;
+            handle.plugin(tauri_plugin_updater::Builder::new().build())?;
             Ok(())
+        })
+        .menu(|handle| {
+            Menu::with_items(
+                handle,
+                &[&Submenu::with_items(
+                    handle,
+                    "App",
+                    true,
+                    &[
+                        &PredefinedMenuItem::about(
+                            handle,
+                            Some(&"About Tweaked"),
+                            Some(AboutMetadata {
+                                name: Some("Tweaked.ai".to_string()),
+                                authors: Some(vec!["Automators".to_string()]),
+                                ..Default::default()
+                            }),
+                        )?,
+                        &PredefinedMenuItem::separator(handle)?,
+                        &MenuItem::new(handle, "Settings", true, Some("cmd+,"))?,
+                        &PredefinedMenuItem::separator(handle)?,
+                        &PredefinedMenuItem::quit(handle, None)?,
+                    ],
+                )?],
+            )
         })
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_process::init())
